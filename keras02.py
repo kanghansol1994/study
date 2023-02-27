4a9ca014708dcd637ab08f2badc5354de97980e5
kanghansol1994 <126450824+kanghansol1994@users.noreply.github.com>
2023-02-27 18:14:49 +0900

    Add files via upload

---
+++ keras02.py
@@ -0,0 +1,16 @@
+#1. 데이터
+import numpy as np
+x = np.array([1,2,3])
+y = np.array([1,2,3])
+
+#2. 모델구성
+import tensorflow as tf
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+model = Sequential()
+model.add(Dense(1, input_dim=1))
+
+#3. 컴파일, 훈련
+model.compile(loss='mse', optimizer='adam')
+model.fit(x, y, epochs=100)
