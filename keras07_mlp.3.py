307e8f332c11dbb5334af6b8662bc92206580517
kanghansol1994 <126450824+kanghansol1994@users.noreply.github.com>
2023-03-02 18:19:25 +0900

    Add files via upload

---
+++ keras07_mlp.3.py
@@ -0,0 +1,32 @@
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+
+#1. 데이터
+x = np.array(
+   [[1,2,3,4,5,6,7,8,9,10],
+    [1,1,1,1,2,1.3,1.4,1.5,1.6,1.4],
+    [9,8,7,6,5,4,3,2,1,0]]
+)
+y = np.array([11, 12, 13, 14, 15, 16, 17, 18 , 19, 20])
+
+#x= x.transpose()
+x= x.T
+
+#2. 모델구성
+model= Sequential()
+model.add(Dense(3, input_dim=3))
+model.add(Dense(5))
+model.add(Dense(4))
+model.add(Dense(3))
+model.add(Dense(1))
+
+#3. 컴파일.훈련
+model.compile(loss='mse', optimizer='adam')
+model.fit(x,y , epochs=600, batch_size=32)
+
+#4. 평가, 예측
+loss=model.evaluate(x,y)
+print('loss= ', loss)
+result=model.predict([[10,1.4,0]])
+print('[10,1.4,0]의 예측값= ', result)
