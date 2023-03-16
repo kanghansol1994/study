9e24f117c5e9c2b4613f993a9df1d719263ddb2a
kanghansol1994 <126450824+kanghansol1994@users.noreply.github.com>
2023-03-16 18:12:18 +0900

    Add files via upload

---
+++ keras32_mnist3_maxpooling.py
@@ -0,0 +1,55 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Conv2D, Flatten,MaxPooling2D
+import numpy as np
+from sklearn.metrics import r2_score, accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+from keras.utils import to_categorical
+#1. 데이터
+(x_train,y_train), (x_test, y_test) = mnist.load_data()
+
+scaler = MinMaxScaler()
+x_train = x_train.reshape(-1,1)
+x_train = scaler.fit_transform(x_train)
+x_test = x_test.reshape(-1,1)
+x_test = scaler.transform(x_test)
+##########실습########
+x_train = x_train.reshape(60000, 28, 28, 1)
+x_test = x_test.reshape(10000, 28, 28, 1) #데이터의 구조만 바뀌는것 순서와,값이 바뀌는게 아님
+print(np.unique(y_train, return_counts=True)) #[0,1,2,3,4,5,6,7,8,9]
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+print(y_train)
+print(y_train.shape)
+#2. 모델구성
+model = Sequential()
+model.add(Conv2D(64, (2,2), padding='same',
+                 input_shape=(28,28,1)))  
+model.add(MaxPooling2D())                        #칸이 중첩되지 않게 설계                  
+model.add(Conv2D(filters=64,             
+                 kernel_size=(2,2), padding='valid',    
+                 activation = 'relu'))  
+model.add(Conv2D(32,2))             
+model.add(Flatten())                    
+model.add(Dense(10, activation='softmax')) 
+
+model.summary()
+
+#3. 컴파일, 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+from tensorflow.python.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='val_acc', patience=10, mode='max',
+                    verbose=1,
+                    restore_best_weights=True,)
+
+model.fit(x_train, y_train, epochs=10, batch_size=5000, verbose=1,
+                 validation_split=0.1, callbacks=[es])
+
+#4. 평가, 예측
+loss = model.evaluate(x_test,y_test)
+print('loss : ', loss)
+
+y_predict = model.predict(x_test)
+acc = accuracy_score(np.argmax(y_test,axis=1), np.argmax(y_predict,axis=1))
+print('acc : ', (acc))
---
+++ keras33_cifar10_cnn.py
@@ -0,0 +1,75 @@
+from tensorflow.keras.datasets import cifar10
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Conv2D, Flatten,MaxPooling2D
+import numpy as np
+from sklearn.metrics import r2_score, accuracy_score
+from sklearn.preprocessing import MinMaxScaler
+from keras.utils import to_categorical
+#1. 데이터
+(x_train,y_train), (x_test, y_test) = cifar10.load_data()
+
+print(x_train.shape, y_train.shape)    #(50000, 32, 32, 3) (50000, 1)
+print(x_test.shape, y_test.shape)      #(10000, 32, 32, 3) (10000, 1)
+
+scaler = MinMaxScaler() #이미지에서는 이렇게 사용가능
+x_train = x_train/255.
+x_test = x_test/255.
+print(np.max(x_train), np.min(x_train)) #1.0 0.0
+
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+print(y_train)
+print(y_train.shape) #(50000,10)
+
+#2. 모델
+model = Sequential()
+model.add(Conv2D(64,5, padding='same', activation='relu', input_shape=(32, 32, 3))),
+model.add(MaxPooling2D())
+model.add(Conv2D(128,5, padding='same', activation='relu')),
+model.add(MaxPooling2D())
+model.add(Conv2D(128,5 , padding='same', activation='relu')),
+model.add(Flatten())
+model.add(Dense(64, activation='relu')),
+model.add(Dense(10, activation='softmax'))
+
+model.summary()
+
+#3. 컴파일, 훈련
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['acc'])
+from tensorflow.python.keras.callbacks import EarlyStopping
+es = EarlyStopping(monitor='val_acc', patience=10, mode='max',
+                    verbose=1,
+                    restore_best_weights=True,)
+
+model.fit(x_train, y_train, epochs=100, batch_size=128, verbose=1,
+                 validation_split=0.1, callbacks=[es])
+
+#4. 평가, 예측
+loss = model.evaluate(x_test,y_test)
+print('loss : ', loss)
+
+y_predict = model.predict(x_test)
+acc = accuracy_score(np.argmax(y_test,axis=1), np.argmax(y_predict,axis=1))
+print('acc : ', (acc))
+
+#######################################################################
+# model = Sequential()
+# model.add(Conv2D(32,4, activation='relu', input_shape=(32, 32, 3))),
+# model.add(MaxPooling2D())
+# model.add(Conv2D(64,4, activation='relu')),
+# model.add(Conv2D(64,4 , activation='relu')),
+# model.add(Flatten())
+# model.add(Dense(64, activation='relu')),
+# model.add(Dense(10, activation='softmax
+# loss :  [0.9224437475204468, 0.6814000010490417]
+# acc :  0.6814
+
+# model = Sequential()
+# model.add(Conv2D(64,4, activation='relu', input_shape=(32, 32, 3))),
+# model.add(MaxPooling2D())
+# model.add(Conv2D(128,4, activation='relu')),
+# model.add(Conv2D(64,4 , activation='relu')),
+# model.add(Flatten())
+# model.add(Dense(64, activation='relu')),
+# model.add(Dense(10, activation='softmax'))
+#add : acc :  0.6949
